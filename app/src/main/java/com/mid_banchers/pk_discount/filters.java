b57ecbc7e3db76f0a50bf6883a89d11079dc0860
um41r254 <um41r.254@gmail.com>
2021-06-07 17:20:31 +0500

    With All Activites complete 2

---
+++ app/src/main/java/com/mid_banchers/pk_discount/filters.java
@@ -0,0 +1,14 @@
+package com.mid_banchers.pk_discount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class filters extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filters);
+    }
+}
--- app/src/main/java/com/mid_banchers/pk_discount/multiple_usage.java
+++ app/src/main/java/com/mid_banchers/pk_discount/multiple_usage.java
@@ -6,9 +6,11 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class multiple_usage extends AppCompatActivity {
 CardView mul_u1;
+ImageView filter_ly;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +20,14 @@ CardView mul_u1;
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(multiple_usage.this , product_detail.class);
+                startActivity(intent);
+            }
+        });
+        filter_ly = findViewById(R.id.filter_ly);
+        filter_ly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(multiple_usage.this,filters.class);
                 startActivity(intent);
             }
         });
