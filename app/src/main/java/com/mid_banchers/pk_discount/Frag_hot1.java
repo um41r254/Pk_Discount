ce2c22f23d742f745d42b86f8d63e242ea4faa89
um41r254 <um41r.254@gmail.com>
2021-06-15 15:11:18 +0500

    with frags and intent

--- app/src/main/java/com/mid_banchers/pk_discount/Frag_hot1.java
+++ app/src/main/java/com/mid_banchers/pk_discount/Frag_hot1.java
@@ -1,15 +1,19 @@
 package com.mid_banchers.pk_discount;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import android.provider.DocumentsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.common.base.FinalizablePhantomReference;
 import com.mid_banchers.pk_discount.R;
 
 public class Frag_hot1 extends Fragment {
@@ -17,6 +21,7 @@ public class Frag_hot1 extends Fragment {
 
 
     public Frag_hot1() {
+
         // Required empty public constructor
     }
 
@@ -24,8 +29,21 @@ public class Frag_hot1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_frag_hot1, container, false);
+        View rootview = inflater.inflate(R.layout.fragment_frag_hot1, container, false);
+
+        CardView cardView = rootview.findViewById(R.id.hot1);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(getActivity(),Product_detail.class);
+                startActivity(intent1);
+            }
+        });
+        return rootview;
     }
+
+
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
--- app/src/main/java/com/mid_banchers/pk_discount/Frag_tre2.java
+++ app/src/main/java/com/mid_banchers/pk_discount/Frag_tre2.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
