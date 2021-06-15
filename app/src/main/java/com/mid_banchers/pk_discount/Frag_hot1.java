89f6e7afbc3c55cb0e49dd79af77755d829ff9f5
um41r254 <um41r.254@gmail.com>
2021-06-15 15:37:28 +0500

    with frags and intent

--- app/src/main/java/com/mid_banchers/pk_discount/Frag_hot1.java
+++ app/src/main/java/com/mid_banchers/pk_discount/Frag_hot1.java
@@ -1,6 +1,7 @@
 package com.mid_banchers.pk_discount;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,16 +10,25 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.provider.DocumentsContract;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.common.base.FinalizablePhantomReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.mid_banchers.pk_discount.R;
 
 public class Frag_hot1 extends Fragment {
 
-
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public Frag_hot1() {
 
@@ -37,6 +47,33 @@ public class Frag_hot1 extends Fragment {
             public void onClick(View v) {
                 Intent intent1 = new Intent(getActivity(),Product_detail.class);
                 startActivity(intent1);
+            }
+        });
+        ImageView imageView = rootview.findViewById(R.id.imageView);
+        db.collection("Products")
+                .whereEqualTo("trending", true)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
+                        for (DocumentSnapshot ds : queryDocumentSnapshots.getDocuments()) {
+
+                            String url = ds.getString("image");
+
+                            Glide.with(getActivity())
+                                    .load(url)
+                                    .into(imageView);
+                            Log.d("TAG", "onSuccess: ");
+
+                        }
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("TAG", "onFailure: " + e.getLocalizedMessage());
+                Toast.makeText(getContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
             }
         });
         return rootview;
